52316252d196ee2a323c2e0b11e187e9b0753244
OoogleBoogle <simattfield@gmail.com>
2016-06-27 08:44:50 -0700

    basic model and db files added

---
+++ database/seed.js
@@ -0,0 +1,32 @@
+var termsArray [
+    {
+        question_pos: 1,
+        german: "Apfel",
+        english: "Apple",
+        definition: "A fruit."
+    },
+    {
+        question_pos: 2,
+        german: "Pferd",
+        english: "Horse",
+        definition: "A quadripedal animal which may or may not kick you to death."
+    },
+    {
+        question_pos: 3,
+        german: "Schwanz",
+        english: "Dick",
+        definition: "A reference to male anatomy as well as a name to call someone."
+    },
+    {
+        question_pos: 4,
+        german: "Arsch",
+        english: "Ass",
+        definition: "The bum."
+    },
+    {
+        question_pos: 5,
+        german: "Morden",
+        english: "To murder (infinitive)",
+        definition: "To impose involuntary and hopefully swift death on someone."
+    }
+]
---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express'),
+    app = express(),
+    path = require('path');
+
+var indexRoute = path.join(__dirname, 'index.html');
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/*', function(req, res) {
+  res.sendFile(indexRoute);
+});
+
+app.set('port', process.env.NODE_PORT || 3000);
+
+app.listen(app.get('port'),function() {
+  console.log("Listeing on Port " + app.get('port'));
+});
