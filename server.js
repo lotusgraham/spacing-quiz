7c4dcb41dbba341887d394f3b453a95bb571d72d
Elias Mason <eliasjmason@gmail.com>
2016-06-27 07:31:04 -0700

    Adds list of terms to server.

---
+++ server.js
@@ -0,0 +1,37 @@
+var termsArray [
+    {
+        german: "Apfel",
+        english: "Apple",
+        definition: "A fruit.",
+        correct: false,
+        memorization: 0
+    },
+    {
+        german: "Pferd",
+        english: "Horse",
+        definition: "A quadripedal animal which may or may not kick you to death.",
+        correct: false,
+        memorization: 0
+    },
+    {
+        german: "Schwanz",
+        english: "Dick",
+        definition: "A reference to male anatomy as well as a name to call someone.",
+        correct: false,
+        memorization: 0
+    },
+    {
+        german: "Arsch",
+        english: "Ass",
+        definition: "The bum.",
+        correct: false,
+        memorization: 0
+    },
+    {
+        german: "Morden",
+        english: "To murder (infinitive)",
+        definition: "To impose involuntary and hopefully swift death on someone.",
+        correct: false,
+        memorization: 0
+    }
+]
