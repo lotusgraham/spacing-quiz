861c1d3ff2b33fc948b4e2d1a39347d95b56c589
OoogleBoogle <simattfield@gmail.com>
2016-06-27 10:37:10 -0700

    Serving static files.

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ var express = require('express'),
     app = express(),
     path = require('path');
 
-var indexRoute = path.join(__dirname, 'index.html');
+var indexRoute = path.join(__dirname, 'build/index.html');
+
+app.use(express.static(path.join(__dirname, 'build/')));
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -10,7 +12,7 @@ app.use(function(req, res, next) {
   next();
 });
 
-app.get('/*', function(req, res) {
+app.get('/', function(req, res) {
   res.sendFile(indexRoute);
 });
 
