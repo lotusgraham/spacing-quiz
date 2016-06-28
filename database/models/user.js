96fbb996b073d6ae26fcb7fff5f6274360fc1e1e
OoogleBoogle <simattfield@gmail.com>
2016-06-28 06:57:07 -0700

    Added score to user model

--- database/models/user.js
+++ database/models/user.js
@@ -5,7 +5,8 @@ var userSchema = new Schema({
   googleID: { type: String, index: true },
   fullName: String,
   firstName: String,
-  avatar: String
+  avatar: String,
+  score: { type: Number, default: 0 }
 });
 
 var User = mongoose.model('User', userSchema);
