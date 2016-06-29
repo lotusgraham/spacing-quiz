d0a9cc26af4937f208abb9ddb978e5ebbd229db4
Elias Mason <eliasjmason@gmail.com>
2016-06-29 08:37:23 -0700

    renames reducer.js to reducers.js.

--- actions.js
+++ actions.js
@@ -25,14 +25,14 @@ var makeGuess = function(guess) {
 };
 
 var NEW_GAME = 'NEW_GAME';
-var newGame = function(){
+var newGame = function() {
     return {
         type: NEW_GAME
     };
 };
 
 var BUTTON_CLICK = 'BUTTON_CLICK';
-var button = function(){
+var buttonClick = function() {
     return {
         type: BUTTON_CLICK
     };
@@ -41,12 +41,33 @@ var button = function(){
 
 var getQuestions = function() {
     return function(dispatch) {
-        var url = 'http://localhost:8081/questions';
+        var url = 'http://localhost:3000/flashcards';
         return fetch(url).then(function(res) {
-            console.log('Array of terms?: ', res);
-        });
-    }
+                console.log('I am the ghost.');
+                if (res.status < 200 || res.status >= 300) {
+                    var error = new Error(response.statusText);
+                    error.res = res;
+                    throw error;
+                }
+                console.log(res);
+                return res;
+            })
+            .then(function(res) {
+                return res.json();
+            })
+            .then(function(data) {
+
+                return dispatch(
+                    getQuestionsSuccess(data)
+                );
+            }).catch(function(error) {
+                return dispatch(
+                    getQuestionsError(error)
+                );
+            })
+    };
 }
+
 
 exports.BUTTON_CLICK = BUTTON_CLICK;
 exports.buttonClick = buttonClick;
@@ -57,5 +78,7 @@ exports.newGame = newGame;
 
 exports.GET_QUESTIONS_SUCCESS = GET_QUESTIONS_SUCCESS;
 exports.getQuestionsSuccess = getQuestionsSuccess;
-exports.GET_SCORE_ERROR = GET_SCORE_ERROR;
+exports.GET_QUESTIONS_ERROR = GET_QUESTIONS_ERROR;
 exports.getQuestionsError = getQuestionsError;
+
+exports.getQuestions = getQuestions;
---
+++ reducers.js
@@ -0,0 +1,7 @@
+var actions = require('./actions');
+var update = require('react-addons-update');
+
+var flashCardsReducer = function(state, action)
+console.log('The state is : ', state);
+
+exports.flashCardsReducer = flashCardsReducer;
