1a377d7b2513cc675d0821a46ba7aa06e60543e0
Elias Mason <eliasjmason@gmail.com>
2016-06-29 11:10:15 -0700

    Adds basic functionality for updating the questions.

--- reducers.js
+++ reducers.js
@@ -1,7 +1,22 @@
 var actions = require('./actions');
 var update = require('react-addons-update');
+var initialState = {
+    questions: [],
+    user: "TestUser"
+}
 
-var flashCardsReducer = function(state, action)
-console.log('The state is : ', state);
+var flashCardsReducer = function(state, action) {
+    state = state || initialState;
+    if (action.type === actions.GET_QUESTIONS_SUCCESS) {
+        var newState = update(state, {
+            questions: {
+                $set: action.questions.questions
+            }
+        });
+        return newState;
+    }
+    return state;
+
+}
 
 exports.flashCardsReducer = flashCardsReducer;
