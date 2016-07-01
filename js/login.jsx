b8c5cf82aefb363c3fe2b5aa25f85567f34c9951
lotusgraham <gwhitley99@gmail.com>
2016-07-01 15:39:29 -0400

    adds login page button

--- js/login.jsx
+++ js/login.jsx
@@ -1,10 +1,34 @@
-import React, {Component} from 'react'
+import React from 'react'
+import RaisedButton from 'material-ui/RaisedButton';
+import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
+// export default class Login extends Component {
+//   render() {
+//     return <a href="/login">Login with Google</a>
+//   }
+// }
 
-export default class Login extends Component {
-  render() {
-    return <a href="/login">Login with Google</a>
+
+
+const style = {
+  margin: 12,
+};
+
+var Login = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <Card style={{width: '60%',
+                    margin: '2rem auto'}}
+            containerStyle={{margin: '5rem'}}>
+          <RaisedButton label="Login with Google" linkButton={true} href="/login" secondary={true} style={style} />
+        </Card>
+      </div>
+    );
   }
-}
+});
+
+export default Login;
+
 
 
 // this.props.state.question.correct
