716b26f8e6e30196f9217679b4c3d1faafdeb4f3
OoogleBoogle <simattfield@gmail.com>
2016-07-31 13:10:35 -0700

    updating token retrieval

--- js/QuestionCard.jsx
+++ js/QuestionCard.jsx
@@ -12,15 +12,14 @@ import Snackbar from 'material-ui/Snackbar';
 
 
 function getQueryVariable(variable) {
-    var query = window.location.search.substring(1);
-    var vars = query.split("&");
-    for (var i=0;i < vars.length;i++) {
-      var pair = vars[i].split("=");
-      if (pair[0] == variable) {
-      return pair[1];
-    }
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i < vars.length;i++) {
+    var pair = vars[i].split("=");
+    return pair[1];
   }
 }
+
 
 
 const styles = {
