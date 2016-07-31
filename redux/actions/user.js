0f0b7f08b804cca78dd0c71d2e0c630909ea05cb
OoogleBoogle <simattfield@gmail.com>
2016-07-31 14:25:12 -0700

    update getting query var

--- redux/actions/user.js
+++ redux/actions/user.js
@@ -1,19 +1,17 @@
 import 'isomorphic-fetch';
 
 function getQueryVariable(variable) {
-    var query = window.location.search.substring(1);
-    var vars = query.split("&");
-    for (var i=0;i < vars.length;i++) {
-      var pair = vars[i].split("=");
-      if (pair[0] == variable) {
-      return pair[1];
-    }
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i=0;i < vars.length;i++) {
+    var pair = vars[i].split("=");
+    return pair[1];
   }
 }
 
 function fetchUser() {
   var headers = new Headers({
-    Authorization: 'Bearer ' + getQueryVariable('accessToken')
+    Authorization: 'Bearer ' + getQueryVariable()
   })
   return function(dispatch) {
     return fetch('/userdetails', {headers: headers}).then(function(res) {
